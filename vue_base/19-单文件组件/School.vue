e7eabc1691270c756923841383fcede7775eee31
ycy <1907830798@qq.com>
2022-03-25 15:55:38 +0800

    my first vue commit

---
+++ vue_base/19-单文件组件/School.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+ //组件交互的代码
+  //export default school分别暴露
+export default {
+    name:'School',//开发者工具最终呈现的名字为School
+    data(){
+        return {
+            name:'山海关',
+            address:'山海'
+        }
+    },
+    methods:{
+        showName(){
+            alert(this.name);
+        }
+    }
+};
+//统一暴露
+  // export { School };
+  // export default School //默认暴露
+</script>
+
+<style>
+    .demo {
+        background-color: pink;
+    }
+</style>
---
+++ vue_base/19-单文件组件/main.js
@@ -0,0 +1,10 @@
+// 引入文件
+import App from './App.vue';
+// 创建VM
+new Vue({
+    el:'#root',
+    template:`<App></App>`,
+    components:{
+        App
+    }
+})
